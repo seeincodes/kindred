3d848158255107c676f7665a942356160ed028a3
“seeinplays” <seeinplays@gmail.com>
2022-04-23 15:27:47 -0600

    notifications smart contract and hardhat

---
+++ contracts/Notification.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Notification {
+
+    struct user {
+        string name;
+        address benefaciary_address;
+        bool vote;
+        string description;
+    }
+
+    // struct owner {
+    //     string name;
+    //     address owner_address;
+    //     bool
+    // }
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import EpnsSDK from "@epnsproject/backend-sdk" 
+
 const ethers = require('ethers')
 const chalk = require('chalk')
 const { Watcher } = require('../../../packages/core-utils/dist/watcher')
@@ -87,7 +89,58 @@ const main = async () => {
   console.log(' got L1->L2 message hash', l1ToL2msgHash)
   const l2Receipt = await watcher.getL2TransactionReceipt(l1ToL2msgHash)
   console.log(' completed Deposit! L2 tx hash:', l2Receipt.transactionHash)
+
+  // EPNS
+  // import required packages
+const EpnsSDK = require("@epnsproject/backend-sdk-staging").default;
+// import required packages
+
+/**
+ * Function to send a notification to users who are to recieve poap link
+ */
+const CHANNEL_PK =
+"0x7a9274a922508b5f1a092a235c3def024b42091388da39408da5d162dc8b4277"; // the private key of the channel
+const CTA = "https://github.com/ethereum-push-notification-service"; // the link to be used as your cta
+/**
+ * Main function
+ */
+(async function Main() {
+// INITIALISE THE SDK
+const epnsSdk = new EpnsSDK(CHANNEL_PK);
+
+// Dire NOTIFICATIONS
+
+// TODO: change notification title and body to suite needs
+const pushNotificationTitle = "Welcome"; //the title which would appear in a push notification, usually could be a shorter version of the actual message
+const pushNotificationBody = "Welcome to Kindred"; //the body which would be displayed in a push notification, usually could be a shorter version of the actual message
+
+const notificationTitle = "Kindred notification"; //the long version of the title which would be displayed in the dApp
+const notificationBody = `Please confirm that the`; // the long version of the body which would be displayed in the dApp
+// TODO: change notification title and body to suite needs
+
+// TODO: change recipient address to the address you wish to use to recieve notifications
+// create forloop, get addresses from smart contract
+const recipientAddress = "0xE6B09537d1A6a5242aB47A05134432335B2cD2A1";
+
+const notificationType = 3; // a notification type of 3, means the notification is a direct message to the specified recipient
+// send the actual notification
+const response = await epnsSdk.sendNotification(
+    recipientAddress, //the recipients of the notification
+    pushNotificationTitle, // push notification title
+    pushNotificationBody, //push notification body
+    notificationTitle, //the title of the notification
+    notificationBody, //the body of the notification //the CTA of the notification, the link which should be redirected to when they click on the notification
+    notificationType,
+    CTA);
+
+console.log({
+    response,
+    message: "Your notification has been sucesfully sent"
+});
+
+})();
 }
+
 
 try {
   main()
