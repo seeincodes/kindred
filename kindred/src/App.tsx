429391d52871d6868416b42f49c3d145a8605fd9
Katia Gilligan <katia.gilligan88@gmail.com>
2022-04-24 02:24:41 +0200

    Implemented Gnosis safe

--- kindred/src/App.tsx
+++ kindred/src/App.tsx
@@ -2,10 +2,27 @@ import React from 'react';
 import './App.css';
 // third-web wallet connection
 import { ConnectMetamaskButtonComponent } from './components/ConnectWallet';
+import { useAddress } from "@thirdweb-dev/react";
+
 import { ethers } from 'ethers';
 // Gnosis Safe Core SDK 
 import EthersAdapter from '@gnosis.pm/safe-ethers-lib';
+import SafeServiceClient, { OwnerResponse } from '@gnosis.pm/safe-service-client';
 import Safe, { SafeFactory, SafeAccountConfig } from '@gnosis.pm/safe-core-sdk';
+
+// TODO: Implement Checking if the user already has an account
+// const isUserExistingClient = async () => {
+//   const address = useAddress();
+//   console.log(address)
+//   const safeService = new SafeServiceClient({
+//     txServiceUrl: 'https://safe-transaction.rinkeby.gnosis.io',
+//     ethAdapter: ethAdapterOwner1
+//   })
+//   const safes: OwnerResponse = await safeService.getSafesByOwner()
+
+// }
+
+// isUserExistingClient();
 
 const createSafe = async (addresses: any) => {
   const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1 })
@@ -28,7 +45,6 @@ const web3Provider = window.ethereum;
 const provider = new ethers.providers.Web3Provider(web3Provider);
 
 const owner1 = provider.getSigner(0);
-
 const ethAdapterOwner1 = new EthersAdapter({
   ethers,
   signer: owner1
@@ -38,11 +54,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <button onClick={() => createSafe(['0x00f4349CC7fE6DE11bCDd00f0455b16cB35878AA','0x0f8135a0eAaDa858C44E674E7fD3A4bb1D083157',])}>Create Safe</button>
-      <ConnectMetamaskButtonComponent/>
+        <button onClick={() => createSafe(['0x00f4349CC7fE6DE11bCDd00f0455b16cB35878AA', '0x0f8135a0eAaDa858C44E674E7fD3A4bb1D083157',])}>Create Safe</button>
+        <ConnectMetamaskButtonComponent />
         <p>
         </p>
-       
+
       </header>
       <main>
       </main>
